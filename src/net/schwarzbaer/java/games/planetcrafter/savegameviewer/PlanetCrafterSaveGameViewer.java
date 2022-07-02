7ecde127c93e4f6a3f5ccd3d833c3c0df51e5560
Hendrik Scholtz <135076120+Hendrik2319@users.noreply.github.com>
2022-07-02 23:45:06 +0000

    JAVA: [Eclipse] - Projekt "PlanetCrafterSaveGameViewer" angelegt
    git-svn-id: file:///G/_svn_repo/programming/JAVA-Projects%20-%20Eclipse/PlanetCrafterSaveGameViewer@6436 8592820e-bdd2-cc16-cb4b-ca06df7e8abd

---
+++ src/net/schwarzbaer/java/games/planetcrafter/savegameviewer/PlanetCrafterSaveGameViewer.java
@@ -0,0 +1,10 @@
+package net.schwarzbaer.java.games.planetcrafter.savegameviewer;
+
+public class PlanetCrafterSaveGameViewer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
