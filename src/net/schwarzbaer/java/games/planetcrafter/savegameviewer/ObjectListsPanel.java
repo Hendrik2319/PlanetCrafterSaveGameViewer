e73264df6b506ebdf2f593a3ad9fda69db5bca30
Hendrik Scholtz <135076120+Hendrik2319@users.noreply.github.com>
2023-03-12 16:00:32 +0000

    JAVA: [Eclipse] - Projekt "PlanetCrafterSaveGameViewer"
    git-svn-id: file:///G/_svn_repo/programming/JAVA-Projects%20-%20Eclipse/PlanetCrafterSaveGameViewer@7031 8592820e-bdd2-cc16-cb4b-ca06df7e8abd

--- src/net/schwarzbaer/java/games/planetcrafter/savegameviewer/ObjectListsPanel.java
+++ src/net/schwarzbaer/java/games/planetcrafter/savegameviewer/ObjectListsPanel.java
@@ -4,7 +4,9 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Point;
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.Locale;
 import java.util.function.Supplier;
 
 import javax.swing.JMenuItem;
@@ -15,6 +17,7 @@ import javax.swing.table.TableCellRenderer;
 import net.schwarzbaer.gui.Tables;
 import net.schwarzbaer.java.games.planetcrafter.savegameviewer.Data.ObjectList;
 import net.schwarzbaer.java.games.planetcrafter.savegameviewer.Data.WorldObject;
+import net.schwarzbaer.java.games.planetcrafter.savegameviewer.ObjectTypes.ObjectType;
 
 class ObjectListsPanel extends AbstractTablePanel<ObjectList, ObjectListsPanel.ObjectListsTableModel.ColumnID> {
 	private static final long serialVersionUID = -1787920497956857504L;
@@ -71,8 +74,10 @@ class ObjectListsPanel extends AbstractTablePanel<ObjectList, ObjectListsPanel.O
 
 		@Override
 		public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int rowV, int columnV) {
-			int rowM = rowV<0 ? -1 : table.convertRowIndexToModel(rowV);
+			int rowM    = rowV   <0 ? -1 : table.convertRowIndexToModel   (rowV   );
+			int columnM = columnV<0 ? -1 : table.convertColumnIndexToModel(columnV);
 			ObjectList row = tableModel.getRow(rowM);
+			ObjectListsTableModel.ColumnID columnID = tableModel.getColumnID(columnM);
 			
 			final Component selectedRendererComponent;
 			
@@ -84,16 +89,27 @@ class ObjectListsPanel extends AbstractTablePanel<ObjectList, ObjectListsPanel.O
 				else
 					return GUI.COLOR_Removal_ByUser;
 			};
-				
-			if (value instanceof Boolean) {
+			
+			if (value instanceof Boolean)
+			{
 				boolean isChecked = (Boolean) value;
 				selectedRendererComponent = boolComp;
 				boolComp.configureAsTableCellRendererComponent(table, isChecked, null, isSelected, hasFocus, null, getCustomBackground);
 				boolComp.setHorizontalAlignment(SwingConstants.CENTER);
 				
-			} else {
+			}
+			else
+			{
 				selectedRendererComponent = standardComp;
-				String valueStr = value==null ? null : value.toString();
+				String valueStr = value != null 
+						? value.toString()
+						: columnID!=null && columnID.isOptional
+							? "<undefined>"
+							: null;
+				
+				if (columnID==ObjectListsTableModel.ColumnID.Filling && value instanceof Double)
+					valueStr = String.format(Locale.ENGLISH, "%1.1f%%", ((Double) value)*100);
+				
 				standardComp.configureAsTableCellRendererComponent(table, null, valueStr, isSelected, hasFocus, getCustomBackground, null);
 				if (value instanceof Number)
 					standardComp.setHorizontalAlignment(SwingConstants.RIGHT);
@@ -108,15 +124,24 @@ class ObjectListsPanel extends AbstractTablePanel<ObjectList, ObjectListsPanel.O
 	static class ObjectListsTableModel extends AbstractTablePanel.AbstractTableModel<ObjectList, ObjectListsTableModel.ColumnID> {
 
 		enum ColumnID implements Tables.SimplifiedColumnIDInterface {
-			id         ("ID"       , Long   .class,  75),
-			NonUniqueID("UnI"      , Boolean.class,  30),
-			twinID     ("Twin"     , Boolean.class,  35),
-			container  ("Container", String .class, 350),
-			size       ("Size"     , Long   .class,  50),
-			worldObjs  ("Content"  , String .class, 600),
+			id         ("ID"          , Long   .class,  75),
+			nonUniqueID("UnI"         , Boolean.class,  30),
+			IsTwinID   ("Twin"        , Boolean.class,  35),
+			container  ("Container"   , String .class, 350),
+			size       ("Size"        , Long   .class,  50),
+			Filling    ("Filling"     , Double .class,  50),
+			worldObjs  ("Content"     , String .class, 400),
+			demandItems("Demand Items", String .class, 200, true),
+			supplyItems("Supply Items", String .class, 200, true),
+			dronePrio  ("Drone Prio"  , Long   .class,  50, true),
 			;
 			private final Tables.SimplifiedColumnConfig cfg;
+			private final boolean isOptional;
 			ColumnID(String name, Class<?> colClass, int width) {
+				this(name, colClass, width, false);
+			}
+			ColumnID(String name, Class<?> colClass, int width, boolean isOptional) {
+				this.isOptional = isOptional;
 				cfg = new Tables.SimplifiedColumnConfig(name, colClass, 20, -1, width, width);
 			}
 			@Override public Tables.SimplifiedColumnConfig getColumnConfig() {
@@ -134,6 +159,7 @@ class ObjectListsPanel extends AbstractTablePanel<ObjectList, ObjectListsPanel.O
 
 		@Override public void setDefaultRenderers() {
 			GeneralTCR renderer = new GeneralTCR(this);
+			table.setDefaultRenderer(Double    .class, renderer);
 			table.setDefaultRenderer(Long      .class, renderer);
 			table.setDefaultRenderer(Boolean   .class, renderer);
 			table.setDefaultRenderer(String    .class, renderer);
@@ -152,17 +178,30 @@ class ObjectListsPanel extends AbstractTablePanel<ObjectList, ObjectListsPanel.O
 
 		@Override protected Object getValueAt(int rowIndex, int columnIndex, ObjectListsTableModel.ColumnID columnID, ObjectList row) {
 			switch (columnID) {
-			case id         : return row.id;
-			case NonUniqueID: return row.nonUniqueID;
-			case twinID     : return data.mapWorldObjects.containsKey(row.id);
-			case container  : return row.container==null ? "--" : row.container.getShortDesc();
-			case size       : return row.size;
-			case worldObjs:
-				//Iterator<String> it = Arrays.stream(row.worldObjIds).mapToObj(n->Integer.toString(n)).iterator();
-				//Iterator<String> it = Arrays.stream(row.worldObjs).map(wo->wo==null ? "<????>" : wo.objType).iterator();
-				Iterator<String> it = row.getContentResume().stream().map(e->String.format("%dx %s", e.getValue(), e.getKey())).iterator();
-				return String.join(", ", (Iterable<String>)()->it);
+				case id         : return row.id;
+				case nonUniqueID: return row.nonUniqueID;
+				case IsTwinID   : return data.mapWorldObjects.containsKey(row.id);
+				case container  : return row.container==null ? "--" : row.container.getShortDesc();
+				case size       : return row.size;
+				case Filling    : return row.worldObjs==null || row.size==0 ? null : row.worldObjs.length / (double) row.size;
+				case worldObjs:
+					//Iterator<String> it = Arrays.stream(row.worldObjIds).mapToObj(n->Integer.toString(n)).iterator();
+					//Iterator<String> it = Arrays.stream(row.worldObjs).map(wo->wo==null ? "<????>" : wo.objType).iterator();
+					Iterator<String> it = row.getContentResume().stream().map(e->String.format("%dx %s", e.getValue(), e.getKey())).iterator();
+					return String.join(", ", (Iterable<String>)()->it);
+				case demandItems: return toString(row.demandItems, row.demandItemsStr);
+				case supplyItems: return toString(row.supplyItems, row.supplyItemsStr);
+				case dronePrio  : return row.dronePrio;
 			}
+			return null;
+		}
+
+		private String toString(ObjectType[] objectTypeArr, String objectTypeArrStr)
+		{
+			if (objectTypeArr!=null)
+				return String.join(", ", (Iterable<String>)()->Arrays.stream(objectTypeArr).map(ot->ot==null ? "<null>" : ot.getName()).iterator());
+			if (objectTypeArrStr!=null)
+				return String.format("\"%s\"", objectTypeArrStr);
 			return null;
 		}
 		
